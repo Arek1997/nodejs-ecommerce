cb86c9dac3b54b88e812bc434b7c3cd811fb0bd5
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-07-17 19:06:22 +0200

    Connect User and Product models

--- app.ts
+++ app.ts
@@ -6,6 +6,8 @@ import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
 import { get404 } from './controllers/error';
 import sequelize from './libs/database';
+import Product from './models/product';
+import User from './models/user';
 
 const app = express();
 
@@ -15,15 +17,44 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(async (req, _, next) => {
+	try {
+		const user = await User.findByPk(1);
+		req.user = user;
+
+		next();
+	} catch (err) {
+		console.log(err);
+	}
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
+	// .sync({ force: true })
 	.sync()
 	.then((result) => {
 		// console.log(result);
+		return User.findByPk(1);
+	})
+	.then((user) => {
+		if (!user) {
+			return User.create({
+				name: 'Arek',
+				email: 'test@test.com',
+			});
+		}
+
+		return user;
+	})
+	.then((user) => {
+		// console.log(user);
 		app.listen(3000);
 	})
 	.catch((err) => {
