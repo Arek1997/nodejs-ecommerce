3b5b139326208699ff88eb808ffbd2ba9ec9c8d0
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-07-22 20:25:05 +0200

    Update Card model / view, add cart-item model

---
+++ models/cart-item.ts
@@ -0,0 +1,15 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../libs/database';
+
+const CartItem = sequelize.define('cartItem', {
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+
+	quantity: DataTypes.INTEGER,
+});
+
+export default CartItem;
--- models/cart.ts
+++ models/cart.ts
@@ -1,7 +1,7 @@
-import Product, { ProductItem } from './product';
-import fs from 'fs';
-import path from 'path';
-import { rootPath } from '../utils';
+import { DataTypes } from 'sequelize';
+import sequelize from '../libs/database';
+
+import { ProductItem } from './product';
 
 interface CartInterface {
 	items: ProductInCart[];
@@ -13,126 +13,13 @@ interface ProductInCart extends ProductItem {
 	amount: number;
 }
 
-const cartPath = path.join(rootPath, 'data', 'cart.json');
+const Cart = sequelize.define('cart', {
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+});
 
-const getCart = async () => {
-	try {
-		const cart: CartInterface = await new Promise((resolve, reject) => {
-			fs.readFile(path.join(cartPath), (err, data) => {
-				if (err) {
-					console.log('err', err);
-					reject(err);
-				} else {
-					const cart = JSON.parse(data.toString());
-					resolve(cart);
-				}
-			});
-		});
-
-		return cart;
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-const addProductToCart = async (cart: CartInterface, productId: string) => {
-	const productExist = cart.items.some((product) => product.id === productId);
-
-	if (productExist) {
-		const existingProduct = cart.items.find((item) => item.id === productId)!;
-		existingProduct.amount++;
-	} else {
-		const product = await Product.getById(productId);
-		cart.items = [...cart.items, { ...product!, amount: 1 }];
-	}
-
-	cart.totalAmount++;
-	const totalPrice = cart.items.reduce(
-		(prev, curr) => prev + Number(curr.price) * curr.amount,
-		0
-	);
-
-	cart.totalPrice = +totalPrice.toFixed(2);
-	try {
-		await new Promise((resolve, reject) => {
-			fs.writeFile(cartPath, JSON.stringify(cart), (err) => {
-				if (err) {
-					console.log('writeFileErr', err);
-					reject(err);
-				} else {
-					resolve('Product added to cart');
-				}
-			});
-		});
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-const removeProductFromCart = async (
-	cart: CartInterface,
-	productId: string
-) => {
-	const productExist = cart.items.some((product) => product.id === productId);
-
-	if (!productExist) {
-		return console.log('Product does not exist');
-	}
-
-	const productToRemove = cart.items.find((item) => item.id === productId)!;
-	const updatedCart = {
-		...cart,
-		items: cart.items.filter((item) => item.id !== productId),
-	};
-
-	updatedCart.totalAmount -= productToRemove.amount;
-	updatedCart.totalPrice = +updatedCart.items
-		.reduce((prev, curr) => prev + Number(curr.price) * curr.amount, 0)
-		.toFixed(2);
-
-	try {
-		await new Promise((resolve, reject) => {
-			fs.writeFile(cartPath, JSON.stringify(updatedCart), (err) => {
-				if (err) {
-					console.log('writeFileErr', err);
-					reject(err);
-				} else {
-					console.log(`Product with id: ${productId} removed from cart`);
-					resolve('Product removed from cart');
-				}
-			});
-		});
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-export default class Cart {
-	static async addProduct(productId: string) {
-		const cart = await getCart();
-		await addProductToCart(cart!, productId);
-	}
-
-	static async removeProduct(productId: string) {
-		const cart = await getCart();
-		await removeProductFromCart(cart!, productId);
-	}
-
-	static async get() {
-		const cart = await getCart();
-
-		return cart!;
-	}
-
-	static async getTotalPrice() {
-		const cart = await getCart();
-
-		return cart!.totalPrice;
-	}
-
-	static async getTotalAmount() {
-		const cart = await getCart();
-
-		return cart!.totalAmount;
-	}
-}
+export default Cart;
