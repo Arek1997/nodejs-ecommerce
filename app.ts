5d0ba886218d0caa41a326054ffb644fb1b53451
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-07-27 20:12:49 +0200

    Re-write Userr Model ti JS Class, insert user to req object

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
 import { get404 } from './controllers/error';
 import mongoConnect from './libs/database';
+import User from './models/user';
 
 const app = express();
 
@@ -16,14 +17,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(async (req, _, next) => {
-	// try {
-	// 	const user = await User.findByPk(1);
-	// 	req.user = user;
-	// 	next();
-	// } catch (err) {
-	// 	console.log(err);
-	// }
-	next();
+	try {
+		const user = await User.findById('64c2ac8e9bd7135c73ae6f60');
+		req.user = user;
+		next();
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 app.use('/admin', adminRoutes);
--- models/user.ts
+++ models/user.ts
@@ -1,23 +1,39 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../libs/database';
+import { ObjectId } from 'mongodb';
+import { getMongoDataBase } from '../libs/database';
+import { ProductItem } from './product';
 
-const User = sequelize.define('user', {
-	id: {
-		type: DataTypes.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true,
-	},
+class User {
+	private name: string;
+	private email: string;
+	private cart: { items: ProductItem[] }; // {items: []}
 
-	name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
+	constructor(name: string, email: string, cart: any) {
+		this.name = name;
+		this.email = email;
+		this.cart = cart;
+	}
 
-	email: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-});
+	async save() {
+		try {
+			await getMongoDataBase().collection('users').insertOne(this);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async addToCart(product: Omit<ProductItem, '_id' | 'id'>) {}
+
+	static async findById(userId: string) {
+		try {
+			const user = await getMongoDataBase()
+				.collection('users')
+				.findOne({ _id: new ObjectId(userId) });
+
+			return user;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+}
 
 export default User;
