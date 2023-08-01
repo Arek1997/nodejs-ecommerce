53bedd7edd3b33d4012835da72609d1b3d21294c
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-08-01 20:33:09 +0200

    Update app.ts

--- app.ts
+++ app.ts
@@ -5,9 +5,9 @@ import bodyParser from 'body-parser';
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
 import { get404 } from './controllers/error';
-// import User, { UserInterface } from './models/user';
 import mongoose from 'mongoose';
 import { MONGODB_URL } from './utils';
+import User from './models/user';
 
 const app = express();
 
@@ -17,30 +17,40 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(async (req, _, next) => {
-// 	try {
-// 		const user = (await User.findById(
-// 			'64c2ac8e9bd7135c73ae6f60'
-// 		)) as unknown as UserInterface;
-// 		const { _id, name, email, cart } = user;
-// 		req.user = new User(name, email, cart, _id);
-// 		next();
-// 	} catch (err) {
-// 		console.log(err);
-// 	}
-
-// });
+app.use(async (req, _, next) => {
+	try {
+		const user = await User.findById('64c945f0f6a79e87c045292e');
+		req.user = user;
+		next();
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(get404);
 
-mongoose
-	.connect(MONGODB_URL)
-	.then(() => {
+const connectMongoose = async () => {
+	try {
+		await mongoose.connect(MONGODB_URL);
+		const user = await User.findOne();
+
+		if (!user) {
+			User.create({
+				name: 'Arek',
+				email: 'test@test.com',
+				cart: {
+					items: [],
+				},
+			});
+		}
+
 		app.listen(3000);
-	})
-	.catch((err) => {
+	} catch (err) {
 		console.log(err);
-	});
+	}
+};
+
+connectMongoose();
