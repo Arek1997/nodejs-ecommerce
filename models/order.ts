287d87660e246cf5c0355bb7f747858b308a7e05
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-08-13 20:07:38 +0200

    Create Order Model

--- models/order.ts
+++ models/order.ts
@@ -1,13 +1,40 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../libs/database';
+import { Schema, Types, model, Document } from 'mongoose';
 
-const Order = sequelize.define('order', {
-	id: {
-		type: DataTypes.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true,
+interface Order extends Document {
+	user: {
+		_id: Types.ObjectId;
+		name: string;
+	};
+
+	products: {
+		product: object;
+		quantity: number;
+	}[];
+}
+
+const orderSchema = new Schema<Order>({
+	user: {
+		_id: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+
+		name: {
+			type: String,
+			required: true,
+		},
 	},
+
+	products: [
+		{
+			type: Object,
+			required: true,
+			quantity: { type: Number, required: true },
+		},
+	],
 });
+
+const Order = model<Order>('Order', orderSchema);
 
 export default Order;
