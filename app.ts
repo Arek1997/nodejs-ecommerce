a9906b88fa5c39d815cb4cfc2b1039163f2f5d79
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-08-20 16:24:24 +0200

    Refactor app.ts use newly created seperated files

--- app.ts
+++ app.ts
@@ -1,10 +1,8 @@
-require('dotenv').config();
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import session from 'express-session';
-const MongoDBStore = require('connect-mongodb-session')(session);
+import csrf from 'csurf';
+import flash from 'connect-flash';
 
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
@@ -12,59 +10,30 @@ import authRoutes from './routes/auth';
 
 import { get404 } from './controllers/error';
 
-import User from './models/user';
+import setSessionMiddleware from './middleware/session';
+import assignUserMiddleware from './middleware/assign-user';
+import addValuesToViews from './middleware/add-values-to-views';
 
-import { MONGODB_URL } from './utils';
+import connectMongoose from './lib/mongoose';
 
 const app = express();
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-	session({
-		secret: process.env.SESSION_SECRET!,
-		resave: false,
-		saveUninitialized: false,
-		cookie: {
-			maxAge: 60 * 60 * 1000, // 1h
-			httpOnly: true,
-		},
-		store: new MongoDBStore({
-			uri: MONGODB_URL,
-			collection: 'sessions',
-		}),
-	})
-);
 
-app.use(async (req, res, next) => {
-	try {
-		res.locals.isAuthenticated = req.session.isLoggedIn;
-
-		if (req.session.isLoggedIn) {
-			req.session.user = User.hydrate(req.session.user);
-		}
-	} catch (err) {
-		console.log(err);
-	} finally {
-		next();
-	}
-});
+app.use(setSessionMiddleware);
+app.use(flash());
+app.use(csrfProtection);
+app.use(assignUserMiddleware);
+app.use(addValuesToViews);
 
 app.use('/admin', adminRoutes);
 app.use(authRoutes);
 app.use(shopRoutes);
 app.use(get404);
 
-const connectMongoose = async () => {
-	try {
-		await mongoose.connect(MONGODB_URL);
-		app.listen(3000);
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-connectMongoose();
+connectMongoose(() => app.listen(3000));
