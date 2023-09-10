1eab119bdde4faf25f158c9c087817f6b7ab33b1
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-09-10 17:03:41 +0200

    Add new mailjet method

--- utils/mailjet.ts
+++ utils/mailjet.ts
@@ -27,3 +27,38 @@ export const signupHandler = async (email: string) => {
 		console.log(err);
 	}
 };
+
+export const passwordResetHandler = async (
+	email: string,
+	token: string,
+	forget: boolean
+) => {
+	try {
+		const request = await mailjet.post('send', { version: 'v3.1' }).request({
+			Messages: [
+				{
+					From: {
+						Email: process.env.MAILJET_DEFAULT_EMAIL,
+					},
+					To: [
+						{
+							Email: email,
+						},
+					],
+					Subject: 'Password Reset',
+					TextPart: 'Test Text',
+					HTMLPart: `<h3>Reset your password</h3>
+        <br />
+        <p>You requested password reset</p>
+        <p>Click this <a href="http://localhost:3000/change-password/${token}/?forget=${forget}">link to reset your password.</a> Link is valid for one hour only.</p>
+        <p>If it's not you, just ignore this message.</p>
+				`,
+				},
+			],
+		});
+
+		console.log('request', request);
+	} catch (err) {
+		console.log(err);
+	}
+};
