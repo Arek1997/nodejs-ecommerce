55172459b6982467f1cc37136511c9db5e839a14
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-09-14 15:05:15 +0200

    Add global express error handling

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import csrf from 'csurf';
 import flash from 'connect-flash';
@@ -7,14 +7,14 @@ import flash from 'connect-flash';
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
 import authRoutes from './routes/auth';
-
-import { get404 } from './controllers/error/get404';
+import errorRoutes from './routes/error';
 
 import setSessionMiddleware from './middleware/session';
 import assignUserMiddleware from './middleware/assign-user';
 import addValuesToViews from './middleware/add-values-to-views';
 
 import connectMongoose from './lib/mongoose';
+import { get500 } from './controllers/error/get500';
 
 const app = express();
 const csrfProtection = csrf();
@@ -31,9 +31,14 @@ app.use(csrfProtection);
 app.use(assignUserMiddleware);
 app.use(addValuesToViews);
 
+//Routing
 app.use('/admin', adminRoutes);
 app.use(authRoutes);
 app.use(shopRoutes);
-app.use(get404);
+app.use(errorRoutes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+	get500(req, res, next);
+});
 
 connectMongoose(() => app.listen(3000));
