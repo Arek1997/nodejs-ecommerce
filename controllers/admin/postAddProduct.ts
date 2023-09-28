919ee322ad0fb3eda6810d4c85dbb3b9464931b7
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-09-28 20:03:08 +0200

    Add image check

--- controllers/admin/postAddProduct.ts
+++ controllers/admin/postAddProduct.ts
@@ -1,12 +1,45 @@
 import { RequestHandler } from 'express';
 import Product, { ProductInterface } from '../../models/product';
+import {
+	ALLOWED_FILE_EXTENSIONS,
+	HTML_FILE_INPUT_NAME_ATTRIBUTE,
+} from '../../utils/multer';
+import { inputFileAcceptedFiles } from '../../utils/consts';
 
 export const postAddProduct: RequestHandler = async (req, res) => {
 	const productData: Omit<ProductInterface, 'userId'> = req.body;
+	const { title, description, price } = productData;
 
+	const image = req.file;
+	console.log('file', image);
+
+	if (!image) {
+		return res.status(422).render('admin/add-product', {
+			title: 'Admin Add Product',
+			path: '/admin/add-product',
+			editMode: false,
+			fileInputNameAttribute: HTML_FILE_INPUT_NAME_ATTRIBUTE,
+			acceptedFiles: inputFileAcceptedFiles,
+			errorMessage: `Image format is incorrect. Allowed formats are: ${ALLOWED_FILE_EXTENSIONS.join(
+				', '
+			)}`,
+			inputs: {
+				title,
+				description,
+				price,
+			},
+		});
+	}
+
+	const imageUrl = '/' + image.path;
 	const loggedUser = req.session.user;
 
-	await Product.create({ ...productData, userId: loggedUser });
+	const dataToStore = {
+		...productData,
+		imageUrl,
+		userId: loggedUser,
+	};
 
-	res.redirect('/products');
+	await Product.create(dataToStore);
+	res.redirect('/admin/products');
 };
--- controllers/admin/postEditProduct.ts
+++ controllers/admin/postEditProduct.ts
@@ -1,16 +1,44 @@
 import { RequestHandler } from 'express';
 import Product, { ProductInterface } from '../../models/product';
+import { inputFileAcceptedFiles } from '../../utils/consts';
+import {
+	ALLOWED_FILE_EXTENSIONS,
+	HTML_FILE_INPUT_NAME_ATTRIBUTE,
+} from '../../utils/multer';
 
 export const postEditProduct: RequestHandler = async (req, res) => {
 	const productId = req.params.id;
 
 	try {
-		const updatedProductData: ProductInterface = req.body;
+		const updatedProductData: Omit<ProductInterface, 'userId'> = req.body;
+		const { title, description, price } = updatedProductData;
 
-		await Product.updateOne(
-			{ _id: productId, userId: req.session.user },
-			{ ...updatedProductData }
-		);
+		const image = req.file;
+
+		if (!image) {
+			return res.status(422).render('admin/add-product', {
+				title: `Edit | ${title}`,
+				path: '/admin/edit-product',
+				editMode: true,
+				fileInputNameAttribute: HTML_FILE_INPUT_NAME_ATTRIBUTE,
+				acceptedFiles: inputFileAcceptedFiles,
+				errorMessage: `Image format is incorrect. Allowed formats are: ${ALLOWED_FILE_EXTENSIONS.join(
+					', '
+				)}`,
+				inputs: {
+					title,
+					description,
+					price,
+				},
+			});
+		}
+
+		const imageUrl = '/' + image.path;
+
+		// await Product.updateOne(
+		// 	{ _id: productId, userId: req.session.user },
+		// 	{ ...updatedProductData, imageUrl }
+		// );
 
 		res.redirect('/admin/products');
 	} catch (error) {
