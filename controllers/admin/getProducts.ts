b5c2d7872aed01eba7521fc1381566d028ef071c
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-11-04 12:28:45 +0100

    Add pagination logic

--- controllers/admin/getProducts.ts
+++ controllers/admin/getProducts.ts
@@ -1,13 +1,23 @@
 import { RequestHandler } from 'express';
 import Product from '../../models/product';
+import { ITEMS_PER_PAGE } from '../../utils/consts';
+import { calculatePaginationPages } from '../../utils/functions';
 
 export const getProducts: RequestHandler = async (req, res) => {
+	const page = req.query?.page || 1;
 	const userId = req.session.user;
-	const productsList = await Product.find({ userId });
+
+	const productsList = await Product.find({ userId })
+		.skip((+page - 1) * ITEMS_PER_PAGE)
+		.limit(ITEMS_PER_PAGE);
+
+	const productsAmount = await Product.countDocuments({ userId });
 
 	res.render('admin/products-list', {
 		productsList,
 		title: 'Admin Products',
 		path: '/admin/products',
+		currentPage: +page,
+		paginationPages: calculatePaginationPages(productsAmount, ITEMS_PER_PAGE),
 	});
 };
--- controllers/shop/getProducts.ts
+++ controllers/shop/getProducts.ts
@@ -1,12 +1,22 @@
 import { RequestHandler } from 'express';
 import Product from '../../models/product';
+import { ITEMS_PER_PAGE } from '../../utils/consts';
+import { calculatePaginationPages } from '../../utils/functions';
 
-export const getProducts: RequestHandler = async (_, res) => {
-	const productsList = await Product.find();
+export const getProducts: RequestHandler = async (req, res) => {
+	const page = req.query?.page || 1;
+
+	const productsList = await Product.find()
+		.skip((+page - 1) * ITEMS_PER_PAGE)
+		.limit(ITEMS_PER_PAGE);
+
+	const productsAmount = await Product.countDocuments();
 
 	res.render('shop/products-list', {
 		productsList,
 		title: 'All Products',
 		path: '/products',
+		currentPage: +page,
+		paginationPages: calculatePaginationPages(productsAmount, ITEMS_PER_PAGE),
 	});
 };
